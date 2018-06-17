1ed18f2ff79c23acec1f16332d1fbd8071d15f49
liuda452967166 <452967166@qq.com>
2018-06-17 16:41:46 +0800

    更新2

--- src/com/atguigu/wugong.java
+++ src/com/atguigu/wugong.java
@@ -4,5 +4,6 @@ public class wugong {
 public static void main(String[] args) {
 	System.out.println("0000000000000000000000000");
 	System.out.println("omsodjasojfoasssssss");
+	System.out.println("3333333333333333333");
 }
 }
