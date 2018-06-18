bd267e0803953f710db3d4812b6f4f669625f780
liuda452967166 <452967166@qq.com>
2018-06-18 15:05:17 +0800

    可以写的好

--- src/com/atguigu/wugong.java
+++ src/com/atguigu/wugong.java
@@ -8,10 +8,8 @@ public static void main(String[] args) {
 	System.out.println("你是个号孩子");
 	System.out.println("snoajidao");
 	System.out.println("niahao ashisdfh");
-//---------------------------------------------------
 	System.out.println("最好的组合");
 	System.out.println("为什么把山了说说");
-	//=====================
 	System.out.println("=============================");
 }
 }
