e5da269cb0984aa863e3ee07dab595d4ac10dbfa
liuda452967166 <452967166@qq.com>
2018-06-18 12:15:21 +0800

    更新

--- src/com/atguigu/wugong.java
+++ src/com/atguigu/wugong.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 	System.out.println("3333333333333333333");
 	System.out.println("你是个号孩子");
 	System.out.println("snoajidao");
+	System.out.println("niahao ashisdfh");
 }
 }
